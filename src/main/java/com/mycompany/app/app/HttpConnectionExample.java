50efa6f81b2fcd4cd89823221b5f090201e60374
Daniel Alejandro AV☄️ <89206637+AlejoCNYT@users.noreply.github.com>
2025-09-10 09:08:30 -0500

    Add files via upload

--- src/main/java/com/mycompany/app/app/HttpConnectionExample.java
+++ src/main/java/com/mycompany/app/app/HttpConnectionExample.java
@@ -1,5 +1,11 @@
 package com.mycompany.app.app;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class HttpConnectionExample {
 
     private static final String USER_AGENT = "Mozilla/5.0";
