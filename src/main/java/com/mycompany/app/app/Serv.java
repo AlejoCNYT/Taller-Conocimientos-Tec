54e3c44ef162971806b914045bca328b2ee52848
Daniel Alejandro AV☄️ <89206637+AlejoCNYT@users.noreply.github.com>
2025-09-10 11:42:43 -0500

    Update Serv.java

--- src/main/java/com/mycompany/app/app/Serv.java
+++ src/main/java/com/mycompany/app/app/Serv.java
@@ -2,51 +2,74 @@ import java.net.*;
 import java.io.*;
 
 public class Serv {
-  public static void main(String[] args) throws IOException {
-   ServerSocket serverSocket = null;
-   try { 
-      serverSocket = new ServerSocket(9000);
-   } catch (IOException e) {
-      System.err.println("Could not listen on port: 35000.");
-      System.exit(1);
-   }
+   private static final String USER_AGENT = "Mozilla/5.0";
+    private static final String GET_URL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=fb&apikey=Q1QZFVJQ21K7C6XM";
 
-   Socket clientSocket = null;
-   try {
-       System.out.println("Listo para recibir ...");
-       clientSocket = serverSocket.accept();
-   } catch (IOException e) {
-       System.err.println("Accept failed.");
-       System.exit(1);
-   }
-   PrintWriter out = new PrintWriter(
-                         clientSocket.getOutputStream(), true);
-   BufferedReader in = new BufferedReader(
-                         new InputStreamReader(clientSocket.getInputStream()));
-   String inputLine, outputLine;
-   while ((inputLine = in.readLine()) != null) {
-      System.out.println("Recibí: " + inputLine);
-      
-      if (!in.ready()) {break; }
-      
-   }
-   outputLine = "HTTP/1.1 200 OK\r\n"
-        + "Content-Type: text/html\r\n"
-         + "\r\n"
-         + "<!DOCTYPE html>\n"
-         + "<html>\n"
-         + "<head>\n"
-         + "<meta charset=\"UTF-8\">\n"
-         + "<title>Title of the document</title>\n"
-         + "</head>\n"
-         + "<body>\n"
-         + "<h1>Mi propio mensaje</h1>\n"
-         + "</body>\n"
-         + "</html>\n";
-    out.println(outputLine);
-    out.close(); 
-    in.close(); 
-    clientSocket.close(); 
-    serverSocket.close(); 
-  }
+    public static void main(String[] args) throws IOException {
+
+        URL obj = new URL(GET_URL);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        //The following invocation perform the connection implicitly before getting the code
+        int responseCode = con.getResponseCode();
+        System.out.println("GET Response Code :: " + responseCode);
+
+        if (responseCode == HttpURLConnection.HTTP_OK) { // success
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            boolean running = true;
+            while (running) {
+                Socket clientSocket = null;
+                try {
+                    System.out.println("Facade ready to receive...");
+                } catch (IOException e) {
+                    System.err.println("Accept failed " + e.getMessage());
+                    System.exit(1);
+                }
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+                    String inputLine, outputLine;
+                    boolean firstLine = true;
+                    String requestStringURI = "";
+                    while ((inputLine = in.readLine()) != null) {
+                        if (firstLine) {
+                            System.out.println("Received: " + inputLine);
+                            requestStringURI = inputLine.split(" ")[1];
+                            firstLine = false;
+                            continue;
+                        }
+                        if (!in.ready()) {
+                            break;
+                        }
+                    }
+                }
+
+                // print result
+                System.out.println(response.toString());
+
+            }
+            
+                
+
+    private static String getErrorPage() {
+        return "HTTP/1.1 404 Not Found\n"
+                + "Content-Type: text/html\n"
+                + "\n"
+                + "<!DOCTYPE html>"
+                + "<html>"
+                + "<head>"
+                + "<meta charset=\"UTF-8\">"
+                + "<title>Not Found</title>\n"
+                + "</head>"
+                + "<body>"
+                + "<h1>404 Not Found</h1>"
+                + "</body>"
+                + "</html>";
+    }
 }
+
